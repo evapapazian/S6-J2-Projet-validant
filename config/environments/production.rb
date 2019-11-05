ad7a1c77f3e17fb1208f29d054693f8172621cd5
evapapazian <55738886+evapapazian@users.noreply.github.com>
2019-11-05 22:40:07 +0100

    modif du nom :host pur avoir le bon eventbrite

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-  config.action_mailer.default_url_options = { :host => 'eventbrite-eva.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'eventbrite-eva2.herokuapp.com' }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
